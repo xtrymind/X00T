83e88caf5ddf732a0605174cb2de13aee61f35d4
Albert I <krascgq@outlook.co.id>
2018-08-20 00:11:52 +0800

    power: Import X00T changes
    * Extracted from S323 kernel source.
    * Fix most of warnings/errors reported by checkpatch.pl.
    * Remove excessive Huaqin comments.
    * Fix function typos from Huaqin.
    * Massive cleanup in smb-lib.c imports.
    * Remove demo charging functions.
    * Either demote to pr_debug or remove excess charging logs.
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -330,6 +330,11 @@ static void msm_restart_prepare(const char *cmd)
 			unsigned long code;
 			int ret;
 			ret = kstrtoul(cmd + 4, 16, &code);
+#ifdef CONFIG_MACH_ASUS_X00T
+			if (!ret && code == 8)
+				qpnp_pon_set_restart_reason(
+					PON_RESTART_REASON_ASUS_UNLOCK);
+#endif
 			if (!ret)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
