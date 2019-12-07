9df31114930073411f2dd2963f87a61a8525b095
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-12-08 06:35:23 +0700

    treewide: disable IPC log creation when CONFIG_IPC_LOGGING is disabled
    generated too much noise
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3967,6 +3967,10 @@ static void *ipc_router_create_log_ctx(char *name)
 {
 	struct ipc_rtr_log_ctx *sub_log_ctx;
 
+#ifndef CONFIG_IPC_LOGGING
+	return NULL;
+#endif
+
 	sub_log_ctx = kmalloc(sizeof(struct ipc_rtr_log_ctx),
 				GFP_KERNEL);
 	if (!sub_log_ctx)
