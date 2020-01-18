fa8b1440256f14d3e1a30e173989ffc566040385
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-01-19 03:14:23 +0700

    Revert "kgsl: use gpu idle timeout for l2pc queue timeout"
    This reverts commit 7ad6813d363029001f0d250fd4fee657f005a400.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -42,8 +42,6 @@ unsigned int adreno_dispatch_time_slice = 25;
  */
 unsigned int adreno_disp_preempt_fair_sched;
 
-extern int idle_timeout;
-
 /* Number of commands that can be queued in a context before it sleeps */
 static unsigned int _context_drawqueue_size = 50;
 
@@ -1480,7 +1478,7 @@ int adreno_dispatcher_queue_cmds(struct kgsl_device_private *dev_priv,
 
 	if (device->pwrctrl.l2pc_update_queue)
 		kgsl_pwrctrl_update_l2pc(&adreno_dev->dev,
-				idle_timeout * 1000);
+				KGSL_L2PC_QUEUE_TIMEOUT);
 
 	/* Add the context to the dispatcher pending list */
 	dispatcher_queue_context(adreno_dev, drawctxt);
